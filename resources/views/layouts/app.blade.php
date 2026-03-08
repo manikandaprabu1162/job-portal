03ac1b6ac00d1fbd6414930971e1af48f0c8a44d
manikandaprabu1162 <manikandaprabu157@gmail.com>
2026-03-08 11:22:55 +0530

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobFinder - Find Your Dream Job</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     {{-- Bootstrap 5 CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
